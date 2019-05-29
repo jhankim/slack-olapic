88c95166e6cc912354617785820545e69d6d5de0
Jae <me@jaehan.com>
2019-05-29 08:59:31 -0400

    oops no incremental commits :/ but finalize app!

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const fetch = require('node-fetch');
 const { App } = require('@slack/bolt');
 
 const app = new App({
@@ -6,34 +7,180 @@ const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET,
 });
 
-// Listens to incoming messages that contain "hello"
-app.message('hello', ({ message, say }) => {
-  // say() sends a message to the channel where the event was triggered
-  say({
-    blocks: [
-      {
-        type: 'section',
-        text: {
-          type: 'mrkdwn',
-          text: `Hey there <@{message.user}>!`,
-        },
-        accessory: {
-          type: 'button',
-          text: {
-            type: 'plain_text',
-            text: 'Click Me',
-          },
-          action_id: 'button_click',
-        },
-      },
-    ],
-  });
-});
-
-app.action('button_click', ({ body, ack, say }) => {
+app.command('/olapic', ({ payload, context, command, ack, say }) => {
   // Acknowledge the action
   ack();
-  say(`<@${body.user.id}> clicked the button`);
+
+  // Start magic
+  const keywordArray = command.text.split(',');
+  const apiPayload = {
+    items_per_page: 20,
+    sort: [
+      {
+        key: 'date_approved',
+        order: 'desc',
+      },
+    ],
+    filters: {
+      keywords: {
+        values: keywordArray,
+        condition: 'or',
+      },
+    },
+  };
+  const tokenAndChannel = {
+    token: context.botToken,
+    channel: payload.channel_id,
+  };
+  fetch('https://content.photorank.me/v1/media/search', {
+    method: 'POST',
+    headers: {
+      Authorization: `ApiKey token="${process.env.OLAPIC_API_KEY}"`,
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(apiPayload),
+  })
+    .then(response => response.json())
+    .then(json => {
+      const mediaList = json.data.media.slice(0, 5);
+      if (mediaList.length === 0) {
+        app.client.chat.postEphemeral({
+          ...tokenAndChannel,
+          user: payload.user_id,
+          text: `Sorry! I couldn't find any photos matching *${command.text}* :slightly_frowning_face:`,
+        });
+        return;
+      }
+      // create blocks
+      let blocks = [
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: `Here's the result for: *${command.text}*\n\n`,
+          },
+        },
+        {
+          type: 'divider',
+        },
+      ];
+      // push media to blocks
+      mediaList.forEach(media => {
+        blocks.push(
+          {
+            type: 'section',
+            text: {
+              type: 'mrkdwn',
+              text: `*Source:* ${media.source}\n*Username*: @${media.user.username}\n*Caption:* ${
+                media.caption
+              }\n*Keywords:* ${media.keywords.join(', ')}`,
+            },
+          },
+          {
+            type: 'image',
+            title: {
+              type: 'plain_text',
+              text: media.images.mobile,
+            },
+            block_id: `${media.id}`,
+            image_url: media.images.mobile,
+            alt_text: 'caption goes here!',
+          },
+          {
+            type: 'actions',
+            elements: [
+              {
+                action_id: `share:${media.source}:${media.user.username}:${media.id}`,
+                type: 'channels_select',
+                placeholder: {
+                  type: 'plain_text',
+                  text: 'Share with a channel',
+                  emoji: true,
+                },
+              },
+            ],
+          }
+        );
+      });
+
+      // post
+      app.client.chat.postEphemeral({
+        ...tokenAndChannel,
+        user: payload.user_id,
+        blocks,
+      });
+    })
+    .catch(err => {
+      say(`${err}`);
+    });
+});
+
+app.action(/^(share:).*/, ({ context, payload, action, ack, say, body }) => {
+  const { action_id, selected_channel } = action;
+  const userId = body.user.id;
+  const mediaSource = action_id.split(':')[1];
+  const userHandle = action_id.split(':')[2];
+  const mediaId = action_id.split(':')[3];
+  const tokenAndChannel = {
+    token: context.botToken,
+    channel: body.container.channel_id,
+  };
+
+  fetch(`https://content.photorank.me/v1/media/?ids=${mediaId}`, {
+    headers: {
+      Authorization: `ApiKey token="${process.env.OLAPIC_API_KEY}"`,
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+  })
+    .then(response => response.json())
+    .then(json => {
+      const media = json.data.media[mediaId];
+      app.client.chat.postEphemeral({
+        ...tokenAndChannel,
+        user: userId,
+        text: `Cool! You just shared *@${userHandle}*'s ${mediaSource} image with <#${selected_channel}> :thumbsup::hugging_face:`,
+      });
+      app.client.chat.postMessage({
+        token: context.botToken,
+        channel: selected_channel,
+        blocks: [
+          {
+            type: 'section',
+            text: {
+              type: 'mrkdwn',
+              text: `<@${userId}> shared an image!\n\n`,
+            },
+          },
+          {
+            type: 'divider',
+          },
+          {
+            type: 'section',
+            text: {
+              type: 'mrkdwn',
+              text: `*Source:* ${media.source}\n*Username*: @${media.user.username}\n*Caption:* ${
+                media.caption
+              }\n*Keywords:* ${media.keywords.join(', ')}`,
+            },
+          },
+          {
+            type: 'image',
+            title: {
+              type: 'plain_text',
+              text: media.images.normal,
+            },
+            block_id: `${media.id}`,
+            image_url: media.images.normal,
+            alt_text: 'caption goes here!',
+          },
+        ],
+      });
+    });
+
+  // Acknowledge action request
+  ack();
 });
 
 (async () => {
